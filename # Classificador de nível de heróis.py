590dcb9616c2c79403f5ec8a35907cfd62f0bb11
Marcos-Andr <marcos30gares07@gmail.com>
2026-09-13 17:36:29 -0300

    Laço de repetição

--- # Classificador de nível de heróis.py
+++ # Classificador de nível de heróis.py
@@ -1,26 +1,30 @@
 # Classificador de nível de heróis
 
-#Utilizei python por estar fazendo o curso em conjunto com a faculdade.
-Nome = input("Digite o nome do herói: ")
-XP = int(input("Digite o XP do herói:"))
-Nivel = ""
-if XP <= 1000: 
-    Nivel = "Ferro"
-elif XP <= 2000:
-    Nivel = "Bronze"
-elif XP <= 5000:
-    Nivel = "Prata"
-elif XP <= 7000:
-    Nivel = "Ouro"
-elif XP <= 8000:
-    Nivel = "Platina"
-elif XP <= 9000:
-    Nivel = "Ascendente"
-elif XP <= 10000:
-    Nivel = "Imortal"  
-else:
-    Nivel = "Radiante"
+while True:
+    Nome = input("Digite o nome do herói: ")
+    XP = int(input("Digite o XP do herói:"))
+    Nivel = ""
+    if XP <= 1000:
+        Nivel = "Ferro"
+    elif XP <= 2000:
+        Nivel = "Bronze"
+    elif XP <= 5000:
+        Nivel = "Prata"
+    elif XP <= 7000:
+        Nivel = "Ouro"
+    elif XP <= 8000:
+        Nivel = "Platina"
+    elif XP <= 9000:
+        Nivel = "Ascendente"
+    elif XP <= 10000:
+        Nivel = "Imortal"
+    else:
+        Nivel = "Radiante"
 
-print("\n===== Informações do Herói =====")
-print(f"Nome: {Nome}")
-print(f"Nível: {Nivel}")
+    print("\n===== Informações do Herói =====")
+    print(f"Nome: {Nome}")
+    print(f"Nível: {Nivel}")
+
+    continuar = input("\nDeseja cadastrar outro herói? (s/n): ")
+    if continuar.lower() != "s":
+        break
